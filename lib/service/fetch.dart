b5ad0ebcafad80af1b386ec1a087b96b6b00b206
AdamMusa <adammusa2222@gmail.com>
2021-04-21 23:53:42 +0100

    implement getConnect functionaly

---
+++ lib/service/fetch.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:get/get.dart';
+
+class Fetch extends GetConnect {
+  Future sendData(String data) async {
+    Response response = await post("", "");
+    return response.body;
+  }
+}
